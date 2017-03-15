e2b9c732b78d5538b1408a25620c458d50075775
Chris <codemedarkone@gmail.com>
2017-03-15 13:19:49 -0600

    Installed devise gem for authentication and added a custom route for login/sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+
+  resources :blogs
+
+  get 'action-animes', to: 'animes#actionanime'
+
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
   resources :animes do
     member do
       get :toggle_status 
     end
   end
 
-  get 'action-animes', to: 'animes#actionanime'
-
-  resources :blogs
-
-  get 'about-me', to: 'pages#about'
-  get 'contact', to: 'pages#contact'
   root 'pages#home'
 end
