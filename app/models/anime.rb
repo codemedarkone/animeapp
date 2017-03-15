16786458ec9882dd15bb9adb27c4c5c1da39ffef
Chris <codemedarkone@gmail.com>
2017-03-14 19:43:14 -0600

    Implemented image defaults

--- app/models/anime.rb
+++ app/models/anime.rb
@@ -1,9 +1,10 @@
 class Anime < ApplicationRecord
+  validates_presence_of :title, :body, :main_image, :thumb_image
   enum status: { draft: 0, published: 1 }
   extend FriendlyId
   friendly_id :title, use: :slugged
 
-  validates_presence_of :title, :body, :main_image, :thumb_image
+
 
   belongs_to :topic
 
@@ -12,5 +13,13 @@ class Anime < ApplicationRecord
   end
 
   scope :action_anime, -> { where(subtitle: "Action Anime") }
+
+ #this runs when something happesn in the form/new action, or creating via scrip
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||="http://placehold.it/350x400"
+    self.thumb_image ||="http://placehold.it/350x200"
+  end
 
 end
